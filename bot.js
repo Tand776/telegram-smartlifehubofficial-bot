c6b21508e90df2781cbb90ef68595580948f18ea
Tand776 <tandohstephen766@gmail.com>
2026-03-22 07:46:59 -0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,26 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '8386488171:AAFuRtJTyCnOrkOnbdNr4gpM4ICDeGrVdmM';
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, "Welcome to SmartLife Hub 🚀\nChoose a channel:", {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: "🌿 Health & Wellness", url: "https://t.me/YourHealthChannel" }],
+        [{ text: "🌱 Herbal Remedies", url: "https://t.me/YourHerbalChannel" }],
+        [{ text: "🎓 Education & Academy", url: "https://t.me/YourAcademyChannel" }],
+        [{ text: "📱 Digital Gadgets", url: "https://t.me/YourGadgetChannel" }],
+        [{ text: "👗 Fashion & Style", url: "https://t.me/YourFashionChannel" }],
+        [{ text: "💻 Affiliate Marketing", url: "https://t.me/YourAffiliateChannel" }],
+        [{ text: "✝ Faith & Inspiration", url: "https://t.me/YourFaithChannel" }],
+        [{ text: "⚽ Sports & Games", url: "https://t.me/YourSportsChannel" }]
+      ]
+    }
+  });
+
+});
